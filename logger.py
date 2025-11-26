0a74e561c536d36c68b0c6ca0dc0f999a54d3ca7
AleksaMCode <aleksamcode@gmail.com>
2025-11-26 15:14:15 +0100

    Reformat code using `black` and `isort`

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
 import logging
 import os
-from datetime import datetime
 from logging.handlers import TimedRotatingFileHandler
 
 LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
+
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
@@ -15,7 +15,7 @@ def get_logger(name: str):
             os.path.join(LOG_DIR, "app.log"),
             when="midnight",
             interval=1,
-            backupCount=7, # keep last 7 days
+            backupCount=7,  # keep last 7 days
             encoding="utf-8",
         )
         handler.suffix = "%Y-%m-%d.log"
